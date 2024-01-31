a4b681530122c7b095c2ebaf26ddb7b35b0e99e5
feng19 <kevin@feng19.com>
2024-01-31 10:31:12 +0800

    release 0.1.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule PhxNewDesktop.MixProject do
   def project do
     [
       app: @app,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
